43ab3b42cde165a6b4a61ef52c3027f86dd64130
Justin Winn <jwinn03@vt.edu>
2025-12-06 02:21:07 -0500

    Added previous page button

--- bluetooth_pedal/src/main.cpp
+++ bluetooth_pedal/src/main.cpp
@@ -5,22 +5,27 @@
 
 BleKeyboard bleKeyboard("ESP32 BLE Pedal", "Me", 100);
 
-const int buttonPin = 4; // GPIO pin for the button
-bool buttonState = false; // Tracks button state
-bool lastButtonState = false;
+const int nextButtonPin = 4; // GPIO pin for the next page button
+const int prevButtonPin = 2; // GPIO pin for the previous page button - optional
+bool nextButtonState = false; // Tracks button state
+bool prevButtonState = false; 
+bool nextLastButtonState = false;
+bool prevLastButtonState = false;
 
 void setup() {
     //Serial.begin(115200);
-    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(nextButtonPin, INPUT_PULLUP);
+    pinMode(prevButtonPin, INPUT_PULLUP);
     bleKeyboard.begin();
 }
 
 void loop() {
     if (bleKeyboard.isConnected()) {
-        buttonState = digitalRead(buttonPin); // Read the button state
+        nextButtonState = digitalRead(nextButtonPin); // Read the button state
+        prevButtonState = digitalRead(prevButtonPin); // Read the button state
 
-        // Check if button is pressed (LOW due to pull-up) and handle state change
-        if (buttonState == LOW && lastButtonState == HIGH) {
+        // Check if button is pressed (LOW due to pull-up) and handle state change for "next" button
+        if (nextButtonState == LOW && nextLastButtonState == HIGH) {
             //Serial.println("Button pressed, typing 'H'");
             //bleKeyboard.print("H"); // Send the character 'H'
             bleKeyboard.write(KEY_RIGHT_ARROW);
@@ -28,8 +33,19 @@ void loop() {
             //bleKeyboard.release(KEY_RIGHT_ARROW);
             delay(50);              // Debounce delay
         }
+        nextLastButtonState = nextButtonState; // Update last button state
 
-        lastButtonState = buttonState; // Update last button state
+        // Check if button is pressed (LOW due to pull-up) and handle state change for "prev" button
+        if (prevButtonState == LOW && prevLastButtonState == HIGH) {
+            //Serial.println("Button pressed, typing 'H'");
+            //bleKeyboard.print("H"); // Send the character 'H'
+            bleKeyboard.write(KEY_RIGHT_ARROW);
+            //bleKeyboard.press(KEY_RIGHT_ARROW)
+            //bleKeyboard.release(KEY_RIGHT_ARROW);
+            delay(50);              // Debounce delay
+        }
+        prevLastButtonState = prevButtonState; // Update last button state
+
     }
     delay(10);
 }
