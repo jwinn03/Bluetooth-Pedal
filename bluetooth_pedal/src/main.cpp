74fe003e385489ded6f4255459b22f0db1c47a76
Justin Winn <jwinn03@vt.edu>
2025-12-06 02:45:35 -0500

    Added serial debug enable preprocessor directive, changed "next" to "fwd" and "prev" to "bck" to reduce confusion with debounce variable names

--- bluetooth_pedal/src/main.cpp
+++ bluetooth_pedal/src/main.cpp
@@ -1,50 +1,58 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
+
+#define ENABLE_SERIAL_DEBUG
+
 // Enable NIMBLE mode to save power?
-// Probably not neccessary, this is already Bluetooth LE though NIMBLE may reduce memory overhead
+// Probably not necessary, this is already Bluetooth LE though NIMBLE may reduce memory overhead
 
 BleKeyboard bleKeyboard("ESP32 BLE Pedal", "Me", 100);
 
-const int nextButtonPin = 4; // GPIO pin for the next page button
-const int prevButtonPin = 2; // GPIO pin for the previous page button - optional
-bool nextButtonState = false; // Tracks button state
-bool prevButtonState = false; 
-bool nextLastButtonState = false;
-bool prevLastButtonState = false;
+const int fwdButtonPin = 4; // GPIO pin for the fwd page button
+const int bckButtonPin = 2; // GPIO pin for the back page button - optional
+bool fwdButtonState = false; // Tracks button state
+bool bckButtonState = false; 
+bool fwdLastButtonState = false;
+bool bckLastButtonState = false;
 
 void setup() {
-    //Serial.begin(115200);
-    pinMode(nextButtonPin, INPUT_PULLUP);
-    pinMode(prevButtonPin, INPUT_PULLUP);
+    #ifdef ENABLE_SERIAL_DEBUG
+    Serial.begin(115200);
+    #endif
+    pinMode(fwdButtonPin, INPUT_PULLUP);
+    pinMode(bckButtonPin, INPUT_PULLUP);
     bleKeyboard.begin();
 }
 
 void loop() {
     if (bleKeyboard.isConnected()) {
-        nextButtonState = digitalRead(nextButtonPin); // Read the button state
-        prevButtonState = digitalRead(prevButtonPin); // Read the button state
+        fwdButtonState = digitalRead(fwdButtonPin); // Read the button state
+        bckButtonState = digitalRead(bckButtonPin); 
 
-        // Check if button is pressed (LOW due to pull-up) and handle state change for "next" button
-        if (nextButtonState == LOW && nextLastButtonState == HIGH) {
-            //Serial.println("Button pressed, typing 'H'");
-            //bleKeyboard.print("H"); // Send the character 'H'
+        // Check if button is pressed (LOW due to pull-up) and handle state change for "fwd" button
+        if (fwdButtonState == LOW && fwdLastButtonState == HIGH) {
+            #ifdef ENABLE_SERIAL_DEBUG
+            Serial.println("Next button pressed");
+            #endif
             bleKeyboard.write(KEY_RIGHT_ARROW);
             //bleKeyboard.press(KEY_RIGHT_ARROW)
             //bleKeyboard.release(KEY_RIGHT_ARROW);
             delay(50);              // Debounce delay
         }
-        nextLastButtonState = nextButtonState; // Update last button state
+        fwdLastButtonState = fwdButtonState; // Update last button state
 
-        // Check if button is pressed (LOW due to pull-up) and handle state change for "prev" button
-        if (prevButtonState == LOW && prevLastButtonState == HIGH) {
-            //Serial.println("Button pressed, typing 'H'");
+        // Check if button is pressed (LOW due to pull-up) and handle state change for "bck" button
+        if (bckButtonState == LOW && bckLastButtonState == HIGH) {
+            #ifdef ENABLE_SERIAL_DEBUG
+            Serial.println("Prev button pressed");
+            #endif
             //bleKeyboard.print("H"); // Send the character 'H'
-            bleKeyboard.write(KEY_RIGHT_ARROW);
-            //bleKeyboard.press(KEY_RIGHT_ARROW)
-            //bleKeyboard.release(KEY_RIGHT_ARROW);
+            bleKeyboard.write(KEY_LEFT_ARROW);
+            //bleKeyboard.press(KEY_LEFT_ARROW)
+            //bleKeyboard.release(KEY_LEFT_ARROW);
             delay(50);              // Debounce delay
         }
-        prevLastButtonState = prevButtonState; // Update last button state
+        bckLastButtonState = bckButtonState; // Update last button state
 
     }
     delay(10);
